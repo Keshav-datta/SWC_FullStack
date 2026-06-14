0a6c4b2be686e334ec884ca8a7c2012b5c06922f
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 19:47:50 +0530

    Add restaurants data to restaurants.js

---
+++ Lecture 5/Swiggy-Zomato-Cart/js/restaurants.js
@@ -0,0 +1,18 @@
+const restaurants = [
+{
+    id:1,
+    name:"Burger Hub",
+    category:"Fast Food",
+    rating:4.5,
+    cuisine:"American",
+    deliveryTime:"25 mins"
+},
+{
+    id:2,
+    name:"Spice Garden",
+    category:"Indian",
+    rating:4.3,
+    cuisine:"North Indian",
+    deliveryTime:"30 mins"
+}
+];
