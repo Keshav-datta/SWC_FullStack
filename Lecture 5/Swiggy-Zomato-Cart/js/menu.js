0475919f3306d7da77c784f55a4b3fbcdbce8c5f
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 19:48:09 +0530

    Create menu.js

---
+++ Lecture 5/Swiggy-Zomato-Cart/js/menu.js
@@ -0,0 +1,20 @@
+const menuItems = [
+{
+    id:1,
+    restaurantId:1,
+    name:"Veg Burger",
+    price:149
+},
+{
+    id:2,
+    restaurantId:1,
+    name:"Cheese Burger",
+    price:199
+},
+{
+    id:3,
+    restaurantId:2,
+    name:"Paneer Butter Masala",
+    price:249
+}
+];
