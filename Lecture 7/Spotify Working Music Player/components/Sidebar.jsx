69faa9870b5fd206eda36e3c163323fd4beeab44
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 22:50:08 +0530

    Create Sidebar.jsx

---
+++ Lecture 7/Spotify Working Music Player/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Spotify</h2>
+
+      <ul>
+        <li>🏠 Home</li>
+        <li>🔍 Search</li>
+        <li>📚 Library</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
