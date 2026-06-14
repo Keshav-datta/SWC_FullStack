9d264ffb2aa5c845888b399c5a8ad1d8055aeb7c
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 19:48:25 +0530

    Update menu.js

--- Lecture 5/Swiggy-Zomato-Cart/js/menu.js
+++ Lecture 5/Swiggy-Zomato-Cart/js/menu.js
@@ -1,20 +1,72 @@
 const menuItems = [
 {
-    id:1,
-    restaurantId:1,
-    name:"Veg Burger",
-    price:149
+id:1,
+name:"Veg Burger",
+price:149
 },
 {
-    id:2,
-    restaurantId:1,
-    name:"Cheese Burger",
-    price:199
+id:2,
+name:"Cheese Burger",
+price:199
 },
 {
-    id:3,
-    restaurantId:2,
-    name:"Paneer Butter Masala",
-    price:249
+id:3,
+name:"French Fries",
+price:99
 }
 ];
+
+const menu =
+document.getElementById("menu");
+
+menuItems.forEach(item =>
+{
+menu.innerHTML += `
+<div class="card">
+
+<h3>${item.name}</h3>
+
+<p>₹${item.price}</p>
+
+<label>
+<input
+type="checkbox"
+value="30"
+>
+Extra Cheese (+₹30)
+</label>
+
+<br><br>
+
+<button
+onclick="addToCart(${item.id})"
+>
+Add To Cart
+</button>
+
+</div>
+`;
+});
+
+function addToCart(id)
+{
+const item =
+menuItems.find(i => i.id === id);
+
+let cart =
+JSON.parse(
+localStorage.getItem("cart")
+) || [];
+
+cart.push({
+...item,
+quantity:1
+});
+
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
+
+alert("Item Added");
+}
