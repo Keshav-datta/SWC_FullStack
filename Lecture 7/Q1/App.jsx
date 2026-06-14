f221bdc2297e439398242d11d40e9394806349b6
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 22:31:35 +0530

    Create App component for data fetching dashboard

---
+++ Lecture 7/Q1/App.jsx
@@ -0,0 +1,14 @@
+import ProductList from "./components/ProductList";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Data Fetching Dashboard</h1>
+
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
