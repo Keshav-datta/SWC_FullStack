67d1f3205bbe6f00cae9cc6805c56956ce1cab35
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 22:58:18 +0530

    Create  ProductCard.jsx

---
+++ Lecture 8/components/ProductCard.jsx
@@ -0,0 +1,41 @@
+import {
+  useContext,
+} from "react";
+
+import {
+  CartContext,
+} from "../context/CartContext";
+
+function ProductCard({
+  product,
+}) {
+  const {
+    addToCart,
+  } = useContext(
+    CartContext
+  );
+
+  return (
+    <div className="card">
+      <h3>
+        {product.name}
+      </h3>
+
+      <p>
+        ₹{product.price}
+      </p>
+
+      <button
+        onClick={() =>
+          addToCart(
+            product
+          )
+        }
+      >
+        Add To Cart
+      </button>
+    </div>
+  );
+}
+
+export default ProductCard;
