8f718aa012476d03cc754b3aaa30cee654c86691
Keshav Datta <keshavdatta23@gmail.com>
2026-06-14 22:38:39 +0530

    Create App component for Checkout Form System

---
+++ Lecture 7/Q3/App.jsx
@@ -0,0 +1,14 @@
+import FormComponent from "./components/FormComponent";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Checkout Form System</h1>
+
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
